66b7820987853c31ed964ec0c055f04a50334e7e
Salamander <1906747819@qq.com>
2020-12-17 18:43:12 +0800

    add project code.

---
+++ Baoxiaohe/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Baoxiaohe
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private const String UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36";
+
+        private const int defaultDevPort = 99678;  // 默认端口
+
+        public App()
+        {
+            Application.Current.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            var settings = new CefSettings
+            {
+                RemoteDebuggingPort = defaultDevPort,
+                CachePath = System.IO.Path.GetFullPath("cache"),
+            };
+            settings.UserAgent = UA;
+            settings.CefCommandLineArgs.Add("enable-media-stream", "1");
+            settings.CefCommandLineArgs.Add("--disable-pinch", "1");
+            settings.CefCommandLineArgs.Add("allow-running-insecure-content", "1");
+            Cef.Initialize(settings);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = e.ExceptionObject as Exception;
+            Console.WriteLine("发生异常：" + exception.Message);
+        }
+
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show("发生异常：" + e.Exception.Message);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Cef.Shutdown();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ Baoxiaohe/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Baoxiaohe
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private const string MAIN_URL = "https://www.baoxiaohe.com/";
+        DispatcherTimer startLogodispatcherTimer = new DispatcherTimer();
+
+        private void myChrome_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+
+        private void myChrome_IsBrowserInitializedChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (myChrome.IsBrowserInitialized)
+            {
+                if (!String.IsNullOrEmpty(MAIN_URL))
+                {
+                    myChrome.Load(MAIN_URL);
+                    startLogodispatcherTimer.Tick += StartLogodispatcherTimer_Tick;
+                    startLogodispatcherTimer.Interval = new TimeSpan(0, 0, 0, 2, 600);
+                    startLogodispatcherTimer.Start();
+                }
+            }
+        }
+
+        private void StartLogodispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            startLogodispatcherTimer.Stop();
+            startArea.Visibility = Visibility.Collapsed;
+            myChrome.Visibility = Visibility.Visible;
+        }
+    }
+}
